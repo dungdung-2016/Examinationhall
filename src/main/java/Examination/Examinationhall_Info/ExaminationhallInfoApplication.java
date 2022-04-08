74cc13000045b975ae2598c9842171ebefae0647
euichanhwang <b611219@g.hongik.ac.kr>
2022-04-08 20:36:14 +0900

    make BaseTimeEntity.java

---
+++ src/main/java/Examination/Examinationhall_Info/ExaminationhallInfoApplication.java
@@ -0,0 +1,13 @@
+package Examination.Examinationhall_Info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationhallInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminationhallInfoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/Examination/Examinationhall_Info/controller/HomeController.java
@@ -0,0 +1,17 @@
+package Examination.Examinationhall_Info.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController {
+
+
+
+
+
+}
