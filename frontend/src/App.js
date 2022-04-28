6319a6c687f44900ae7c030893eb46b4315965c0
euichanhwang <b611219@g.hongik.ac.kr>
2022-04-28 14:19:38 +0900

    프론트엔드 부분 추가

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { BrowserRouter, Route, Routes, Link, NavLink } from "react-router-dom";
+import './App.css';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid';
+import { Checkbox, TextField } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Home from './Home';
+import NotFound from './NotFound';
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </BrowserRouter>
+
+
+    </div>
+  );
+}
+
+export default App;
